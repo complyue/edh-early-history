96476fad3da498436d68ccaa1c1ee45beb50f991
ComplYue <compl.yue@icloud.com>
2019-12-03 17:44:33 +0800

    allow Ctrl^D to quit repl

--- edh/repl/Main.hs
+++ edh/repl/Main.hs
@@ -2,7 +2,10 @@ module Main where
 
 import           RIO
 import           Data.Text.IO                   ( putStrLn )
+
+import           Evaluator.Types                ( createEmptyState )
 import           Repl                           ( repl )
 
 main :: IO ()
-main = putStrLn ("The Monkey programming language REPL" :: Text) >> repl
+main = putStrLn ("(EDHi)" :: Text) >> void (createEmptyState >>= repl)
+
--- edh/repl/Repl.hs
+++ edh/repl/Repl.hs
@@ -11,15 +11,11 @@ import           Parser                         ( parse )
 import           Evaluator                      ( evalWithState )
 import           Evaluator.Types                ( EvalError
                                                 , EvalState
-                                                , createEmptyState
                                                 )
 import           Evaluator.Object               ( Object )
 import           Common.ParserT                 ( ParserError )
 import           System.Console.Haskeline
 import qualified GHC.Show                      as G
-
-loop :: (a -> IO a) -> a -> IO a
-loop io = io >=> loop io
 
 read :: IO (Maybe Text)
 read = runInputT defaultSettings $ (fmap . fmap) T.pack $ getInputLine "> "
@@ -39,20 +35,17 @@ evaluate state input = do
             Left  err -> return $ Left (E err)
         Left err -> return $ Left (P err)
 
-rep :: EvalState -> IO EvalState
-rep state = do
-    maybeText <- read
-    case maybeText of
-        Just text | not $ T.null text -> do
-            result <- evaluate state text
+repl :: EvalState -> IO EvalState
+repl state = read >>= \case
+    Nothing   -> return state
+    Just text -> case T.strip text of
+        ""    -> repl state
+        text' -> do
+            result <- evaluate state text'
             case result of
                 Left err -> do
-                    putStrLn (tshow err)
-                    return state
+                    putStrLn $ tshow err
+                    repl state
                 Right (object, state') -> do
-                    putStrLn $ (tshow object)
-                    return state'
-        _ -> return state
-
-repl :: IO ()
-repl = (createEmptyState >>= loop rep) $> ()
+                    putStrLn $ tshow object
+                    repl state'
