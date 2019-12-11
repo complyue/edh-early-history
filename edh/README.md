745e6922d44d33999c67cc8b6c0bc8fe74fad08b
ComplYue <compl.yue@icloud.com>
2019-12-11 15:41:00 +0800

    remove monkey-hs stuff as not used anymore, to compile faster

--- edh/README.md
+++ edh/README.md
@@ -1,1 +1,11 @@
-# Đ - the language/runtime for Event Distributing & Hosting
+# Đ - (WIP) a modern Object layer spliced with Haskell
+
+Missing Python/JavaScript or even Go in shifting to
+Haskell? Now you can have Edh, with many goodies from
+those languages but running in Haskell.
+
+# Acknowledgement
+
+The development of Edh was started from cloning
+[monkey-hs](https://github.com/utatti/monkey-hs)
+by Hyunje Jun, but later re-written entirely.
--- edh/pub/Language/Edh/EHI.hs
+++ edh/pub/Language/Edh/EHI.hs
@@ -1,25 +1,121 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments #-}
+
 -- | Edh Host Interface
 --
 -- With Haskell as the host language, Edh as the guest language,
--- this defines the interface for Edh hosting programs to create
+-- this defines the interface for host code in Haskell to create
 -- & control embed Edh worlds, and to splice host (typically
 -- side-effects free, i.e. pure, and performant) functions in
 -- Haskell, with object-orient, while highly mutable (thus
 -- world-changing), but not-so-performant (as being interpreted)
 -- procedures in Edh.
 module Language.Edh.EHI
-    ( EdhValue(..)
+    (
+    -- * Monadic API
+      EdhInterpreter
+    , EdhSession
+    -- ** Interpreter entry point    
+    , runEdh
+    , runEdhWithoutBatteries
+    -- ** Session entry point
+    , runEdhSession
+    -- ** The eval function
+    , edhEval
+
+    -- ** Exceptions
+    , InterpretError(..)
+
+    -- * Pure API
+    , createEdhWorld
+    , installEdhBatteries
+    , runEdhModule
+    , evalEdhSource
+
+    -- * Data types
+    -- ** End values
+    , EdhValue(..)
     , nil
     , true
     , false
     , nan
     , inf
     , D.Decimal(..)
+    -- ** Reflection
+    , EdhWorld
+    , Symbol
+    , Entity
+    , AttrKey
+    , Dict
+    , ItemKey
+    , Object
+    , Class
+    , Method
+    , Module
+    , Iterator
+    -- ** World changing tools
+    , declareEdhOperators
+    , putEdhAttrs
+    , putEdhAttr
     )
 where
 
+import           Prelude
+
+import           Control.Exception
+import           Control.Monad
+import           Control.Monad.Fail
+import           Control.Monad.Reader
+
+import           Data.Text                     as T
+
 import qualified Data.Lossless.Decimal         as D
 
-import           Language.Edh.Runtime
+import           Language.Edh.Control
+import           Language.Edh.Interpreter
 import           Language.Edh.Batteries
+import           Language.Edh.Runtime
+import           Language.Edh.AST
+
+
+edhEval :: Text -> EdhSession EdhValue
+edhEval code = do
+    (world, modu) <- ask
+    liftIO $ evalEdhSource world modu code >>= \case
+        Left  err -> throwIO err
+        Right v   -> return v
+
+runEdhSession
+    :: ModuleId
+    -> Text
+    -> EdhSession a
+    -> EdhInterpreter (Either InterpretError a)
+runEdhSession moduId moduSource (EdhSession (ReaderT f)) = do
+    world <- ask
+    runEdhModule world moduId moduSource >>= \case
+        Left  err  -> return $ Left err
+        Right modu -> liftIO $ tryJust Just $ f (world, modu)
+
+
+runEdh :: MonadIO m => EdhInterpreter a -> m a
+runEdh (EdhInterpreter (ReaderT f)) = liftIO $ do
+    world <- createEdhWorld
+    installEdhBatteries world
+    f world
+
+runEdhWithoutBatteries :: MonadIO m => EdhInterpreter a -> m a
+runEdhWithoutBatteries (EdhInterpreter (ReaderT f)) =
+    liftIO $ createEdhWorld >>= f
+
+
+newtype EdhSession a = EdhSession { unEdhS :: ReaderT (EdhWorld,Module) IO a }
+    deriving (Functor, Applicative, Monad,
+        MonadReader (EdhWorld, Module),
+        MonadIO, MonadFail)
+
+newtype EdhInterpreter a = EdhInterpreter { unEdh :: ReaderT EdhWorld IO a }
+    deriving (Functor, Applicative, Monad,
+        MonadReader EdhWorld,
+        MonadIO, MonadFail)
 
