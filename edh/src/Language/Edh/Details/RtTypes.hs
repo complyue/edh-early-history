0bde2b5bb05f490990ebcda8f09e2bbcd205d867
Compl Yue <compl.yue@icloud.com>
2019-12-29 16:39:26 +0800

    upd

--- edh/src/Language/Edh/Details/RtTypes.hs
+++ edh/src/Language/Edh/Details/RtTypes.hs
@@ -500,7 +500,7 @@ instance Show EdhValue where
 
   show (EdhHostProc v) = show v
   show (EdhHostOper prec (HostProcedure pn _)) =
-    "[hostop: " ++ show prec ++ " " ++ nm ++ " ]"
+    "[hostop: " ++ show prec ++ " (" ++ nm ++ ") ]"
     where nm = unsafePerformIO $ peekCString pn
 
 
