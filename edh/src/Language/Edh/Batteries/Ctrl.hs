de62fe3ac0b25e82febd0612b578d6df9b3b9b29
Compl Yue <compl.yue@icloud.com>
2020-01-02 12:35:15 +0800

    brancher improve

--- edh/src/Language/Edh/Batteries/Ctrl.hs
+++ edh/src/Language/Edh/Batteries/Ctrl.hs
@@ -14,7 +14,10 @@ import           Language.Edh.AST
 import           Language.Edh.Runtime
 
 
--- | operator (->)
+-- | operator (->) - the brancher, if its left-hand matches, early stop its
+-- enclosing code block (commonly a case-of block, but other blocks as well),
+-- with eval-ed result of its right-hand, unless the right-hand result is
+-- `fallthrough`
 branchProc :: EdhProcedure
 branchProc (PackSender [SendPosArg !lhExpr, SendPosArg !rhExpr]) that _ !exit =
   do
@@ -22,6 +25,19 @@ branchProc (PackSender [SendPosArg !lhExpr, SendPosArg !rhExpr]) that _ !exit =
     let !callerCtx@(Context _ _ _ !ctxMatch _) = edh'context pgs
         !callerScope                           = contextScope callerCtx
     case lhExpr of
+      -- | recognize `_` as similar to the wildcard pattern match in Haskell,
+      -- it always matches
+      AttrExpr (DirectRef (NamedAttr "_")) ->
+        evalExpr that rhExpr $ \(that', scope', rhVal) -> exitEdhProc
+          exit
+          ( that'
+          , scope'
+          , case rhVal of
+            EdhFallthrough -> EdhFallthrough
+            _              -> EdhCaseClose rhVal
+          )
+      -- | guarded condition does not match against target in context, but
+      -- whether itself is true
       PrefixExpr Guard guardedExpr ->
         evalExpr that guardedExpr $ \(_, _, predValue) -> if predValue /= true
           then exitEdhProc exit (that, callerScope, EdhFallthrough)
@@ -33,6 +49,7 @@ branchProc (PackSender [SendPosArg !lhExpr, SendPosArg !rhExpr]) that _ !exit =
               EdhFallthrough -> EdhFallthrough
               _              -> EdhCaseClose rhVal
             )
+      -- | match against the target in context
       _ -> evalExpr that lhExpr $ \(_, _, lhVal) -> if lhVal /= ctxMatch
         then exitEdhProc exit (that, callerScope, EdhFallthrough)
         else evalExpr that rhExpr $ \(that', scope', rhVal) -> exitEdhProc
