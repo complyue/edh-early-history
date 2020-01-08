8c77f7c4d1a205e49a1c5263a9629a0882b94462
ComplYue <compl.yue@icloud.com>
2020-01-08 17:50:02 +0800

    upd

--- edh/pub/Language/Edh/EHI.hs
+++ edh/pub/Language/Edh/EHI.hs
@@ -1,37 +1,89 @@
 
 -- | Edh Host Interface
 --
--- With Haskell as the host language, Edh as the guest language,
+-- With Haskell as the host language, Edh as the surface language,
 -- this defines the interface for host code in Haskell to create
--- & control embed Edh worlds, and to splice host (typically
--- side-effects free, i.e. pure, and performant) functions in
--- Haskell, with object-orient, while highly mutable (thus
--- world-changing), but not-so-performant (as being interpreted)
--- procedures in Edh.
+-- & control embedded Edh worlds, and to splice host (typically
+-- side-effects free, i.e. pure, and fast-in-machine-speed)
+-- functions, wrapped as host procedures, with procedures written
+-- in Edh, those do arbitrary manipulations on arbitrary objects
+-- in the world, well, less speedy as being interpreted.
 module Language.Edh.EHI
   (
+    -- * Language infrastructure
+    -- ** Exceptions
+    InterpretError(..)
+    -- ** Logging interface
+  , EdhLogger(..)
+  , LogLevel
+  , defaultEdhLogger
+    -- ** Host arts making tools
+  , mkSymbol
+  , mkHostProc
+  , mkHostOper
+  , declareEdhOperators
+  , installEdhAttrs
+  , installEdhAttr
+    -- ** Value system
+  , edhTypeOf
+  , edhValueStr
+  , edhValueNull
+    -- ** Object system
+  , lookupEdhCtxAttr
+  , lookupEdhObjAttr
+  , resolveEdhCtxAttr
+  , resolveEdhObjAttr
+  , resolveEdhInstance
+    -- ** Runtime system
+  , EdhWorld(..)
+  , EdhRuntime(..)
+  , EdhProg(..)
+  , EdhProgState(..)
+  , EdhTxTask(..)
+  , Context(..)
+  , Scope(..)
+  , runEdhProg
+  , forkEdh
+    -- ** Event processing
+  , newEventSink
+  , subscribeEvents
+  , publishEvent
+    -- ** Runtime error
+  , getEdhErrorContext
+  , throwEdh
+  , throwEdhSTM
+    -- ** CPS helpers
+  , contEdhSTM
+  , exitEdhSTM
+  , exitEdhProc
+  , waitEdhSTM
+  , edhNop
+
     -- * Monadic API
-    EdhProgram
+  , EdhProgram
   , EdhSession
-    -- ** Interpreter entry point    
   , runEdh
   , runEdhWithoutBatteries
-    -- ** Session entry point
   , runEdhSession
-    -- ** The eval function
   , evalEdh
-
-    -- ** Exceptions
-  , InterpretError(..)
 
     -- * IO API
   , createEdhWorld
-  , defaultEdhLogger
   , installEdhBatteries
   , runEdhModule
   , evalEdhSource
 
     -- * Data types
+    -- ** Host artifacts for the splice
+  , HostProcedure(..)
+  , EdhGenrCaller(..)
+  , Class(..)
+  , Method(..)
+  , Operator(..)
+  , GenrDef(..)
+  , Interpreter(..)
+    -- ** Event infrastructure
+  , EventSink(..)
     -- ** End values
   , EdhValue(..)
   , nil
@@ -41,19 +93,16 @@ module Language.Edh.EHI
   , inf
   , D.Decimal(..)
     -- ** Structured values
-  , EdhWorld
-  , Symbol
-  , Entity
-  , AttrKey
-  , Dict
-  , ItemKey
-  , Object
-  , Class
-  , Method
-    -- ** World changing tools
-  , declareEdhOperators
-  , installEdhAttrs
-  , installEdhAttr
+  , Symbol(..)
+  , Entity(..)
+  , AttrKey(..)
+  , Dict(..)
+  , ItemKey(..)
+  , List(..)
+  , ArgsPack(..)
+  , Object(..)
+    -- ** Reflectives
+  , module AST
   )
 where
 
@@ -70,7 +119,8 @@ import           Language.Edh.Control
 import           Language.Edh.Interpreter
 import           Language.Edh.Batteries
 import           Language.Edh.Runtime
-import           Language.Edh.AST
+import           Language.Edh.Event
+import           Language.Edh.AST              as AST
 
 
 evalEdh :: Text -> EdhSession EdhValue
